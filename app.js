4c6eb7def374cb2bb10e51610edb03b1669d8a83
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-11-30 03:06:14 +0300

    fix: added attribute alt according to advanced level rule 1.2 in index.html and app.js

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ var createNewTaskElement=function(taskString){
     //button.delete
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
+    deleteButtonImg.alt = "delete";
 
     label.innerText=taskString;
     label.className='task';
